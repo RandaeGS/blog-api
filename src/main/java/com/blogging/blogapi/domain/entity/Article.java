2eb3a0c705268e40500be2869fe04a28936ad1d6
RandaeGS <garciasanchezrd@gmail.com>
2024-12-19 17:25:46 -0400

    init article entity

---
+++ src/main/java/com/blogging/blogapi/domain/entity/Article.java
@@ -0,0 +1,21 @@
+package com.blogging.blogapi.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String content;
+    private String authorName;
+    private Date publishDate;
+}
